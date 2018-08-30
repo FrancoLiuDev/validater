0bde61a866f2c5484e78d4bfcb17ada1e140a494
Franco <franco.liu@leediancn.com>
2018-08-30 10:52:20 -0600

    update

--- validater/strValidater.js
+++ validater/strValidater.js
@@ -2,6 +2,9 @@ class Validator {
     static isDigiOnly(str) {
         return /^\d+$/.test(str)
     }
+    static isStringValid(str) {
+        return /^\d+$/.test(str)
+    }
 }
 
 export default Validator
