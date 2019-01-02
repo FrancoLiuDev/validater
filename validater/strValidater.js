8673c347d064f1a0be030a5617e5a0a0af2a3b56
franco <francoliu1979@gmail.com>
2019-01-02 23:15:57 +0800

    update

--- validater/strValidater.js
+++ validater/strValidater.js
@@ -1,26 +1,23 @@
 class Validator {
-    static isDigiOnly(str) {
-        return /^\d+$/.test(str)
-    }
-    static isStringValidLenth(len) {
-        return
-    }
-    static isValidEmail(str) {
-        var emailRule = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
-        if (str.search(emailRule) != -1)
-            return true
-        else
-            return false
-    }
-    static equalLength(str, lenth) {
-        return str.length - lenth
-    }
-    static equalLenRange(str, from, end) {
-        if (str.length - from < 0) return (str.length - from)
-        if (str.length - end > 0) return (str.length - end)
-        return 0
-    }
-
+  static isDigiOnly(str) {
+    return /^\d+$/.test(str)
+  }
+  static isStringValidLenth(len) {
+    return
+  }
+  static isValidEmail(str) {
+    var emailRule = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/
+    if (str.search(emailRule) != -1) return true
+    else return false
+  }
+  static equalLength(str, lenth) {
+    return str.length - lenth
+  }
+  static equalLenRange(str, from, end) {
+    if (str.length - from < 0) return str.length - from
+    if (str.length - end > 0) return str.length - end
+    return 0
+  }
 }
 
 export default Validator
